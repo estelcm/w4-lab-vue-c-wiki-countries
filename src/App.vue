2d19964c3bb39967bfd3819f04ffca4a507c52d7
Estel CM <estel_927@hotmail.com>
2022-12-08 16:27:52 +0100

    itertaion 0 to 1.2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app">
-
+    <Navbar />
+    <Countries />
+    <router-view />
   </div>
 </template>
 
 <script>
-
+import Navbar from "./components/Navbar.vue";
+import Countries from "../public/countries.json";
+export default {
+  components: { Navbar, Countries }
+}
 </script>
 
 <style>
---
+++ src/components/CountriesList.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container" v-for="Countrie,index of Countries">
+        <!-- Bootstrap row wrapper div -->
+        <div class="row">
+            <div class="col-5" style="max-height: 90vh; overflow: scroll">
+                <div class="list-group">
+                    <router-link class="list-group-item list-group-item-action" to="/list/ABW">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/aw.png" />
+                        <p>Aruba</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/AFG">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/af.png" />
+                        <p>Afghanistan</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/AGO">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ao.png" />
+                        <p>Angola</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/AIA">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ai.png" />
+                        <p>Anguilla</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ALA">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ax.png" />
+                        <p>Åland Islands</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ALB">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/al.png" />
+                        <p>Albania</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/AND">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ad.png" />
+                        <p>Andorra</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ARE">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ae.png" />
+                        <p>United Arab Emirates</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ARG">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/ar.png" />
+                        <p>Argentina</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ARM">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/am.png" />
+                        <p>Armenia</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ASM">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/as.png" />
+                        <p>American Samoa</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ATA">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/aq.png" />
+                        <p>Antarctica</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/FLK">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/fk.png" />
+                        <p>Falkland Islands</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action active" to="/list/FRA">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/fr.png" />
+                        <p>France</p>
+                    </router-link>
+                    <router-link class="list-group-item list-group-item-action" to="/list/ZWE">
+                        <img src="https://flagpedia.net/data/flags/icon/72x54/zw.png" />
+                        <p>Zimbabwe</p>
+                    </router-link>
+                </div>
+            </div>
+
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["Countries"],
+
+}</script>
