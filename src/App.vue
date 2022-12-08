c1a0064850e36467b53fbb0de742652ea9a519df
Estel CM <estel_927@hotmail.com>
2022-12-08 21:38:47 +0100

    it 3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="app">
     <Navbar />
-    <Countries />
+   
     <router-view />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Countries from "../public/countries.json";
+
 export default {
-  components: { Navbar, Countries }
+  components: { Navbar,}
 }
 </script>
 
