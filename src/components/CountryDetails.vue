97f7d7a81c9eab6f844066b980df346b796f0d55
Estel CM <estel_927@hotmail.com>
2022-12-09 14:15:23 +0100

    final iteration

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="col-7">
-        <img src="https://restcountries.eu/data/fra.svg" alt="country flag" style="width: 300px" />
-        <h1>France</h1>
+        <img :src="'https://www.geonames.org/flags/x/' + showCountry.alpha2Code.toLowerCase() + '.gif'"
+         alt="country flag" style="width: 300px" />
+        <h1>{{showCountry.name.common}}</h1>
         <table class="table">
             <thead></thead>
             <tbody>
                 <tr>
                     <td style="width: 30%">Capital</td>
-                    <td>Paris</td>
+                    <td>{{showCountry.capital[0]}}</td>
                 </tr>
                 <tr>
                     <td>Area</td>
                     <td>
-                        551695 km <sup>2</sup>
+                       {{showCountry.area}}  km <sup>2</sup>
                     </td>
                 </tr>
                 <tr>
                     <td>Borders</td>
                     <td>
-                        <ul>
-                            <li><a href="/AND">Andorra</a></li>
-                            <li><a href="/BEL">Belgium</a></li>
-                            <li><a href="/DEU">Germany</a></li>
-                            <li><a href="/ITA">Italy</a></li>
-                            <li><a href="/MCO">Monaco</a></li>
-                            <li><a href="/ESP">Spain</a></li>
-                            <li><a href="/CHE">Switzerland</a></li>
+                        <ul v-for="(country,index) in showCountry.borders" :key="index">
+                            <li>
+                            <routerLink :to="`/${showCountry.alpha3Code}`">{{country}}
+                            </routerLink>
+                            </li> 
+                            <!-- <li><a href="/BEL"></a></li>
+                            <li><a href="/DEU"></a></li>
+                            <li><a href="/ITA"></a></li>
+                            <li><a href="/MCO"></a></li>
+                            <li><a href="/ESP"></a></li>
+                            <li><a href="/CHE"></a></li> -->
                         </ul>
                     </td>
                 </tr>
@@ -44,17 +48,34 @@ export default {
     }
 
    },
-    computed: {
-        country() {
+   computed:{
 
-        }
-    },
-    mounted() {
-        // NO se tiene que hacer en mounted
-        console.log(this.$route.params)
+    showCountry(){
+const countryId= this.$route.params.alpha3Code;
+return this.countries.find(function(country){
+    if(country.alpha3Code === countryId){
+        return true;
     }
+    else {
+        return false;
+    }
+    });
+    }
+}
+    
+    // // showCountry() {
+    //    return this.countries.find((project)=>
+    //    (country ===this.$route.params.alpha3Code)
+    //    );
+    // }
+  } ;
+
+    // mounted() {
+    //     // NO se tiene que hacer en mounted
+    //     console.log(this.$route.params)
+    // }
 
  
 
 
-}</script>
+</script>
